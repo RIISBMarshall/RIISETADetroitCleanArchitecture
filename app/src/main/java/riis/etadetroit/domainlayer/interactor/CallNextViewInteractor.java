8d501bf785722dc11a10436eca782007ea367ed5
Brian Marshall <bmarshall@RIIS.com>
2017-02-10 16:04:56 -0500

    moved intent code to CallNextViewInteractor

---
+++ app/src/main/java/riis/etadetroit/domainlayer/interactor/CallNextViewInteractor.java
@@ -0,0 +1,46 @@
+package riis.etadetroit.domainlayer.interactor;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.ActivityOptionsCompat;
+import android.support.v4.util.Pair;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import riis.etadetroit.R;
+import riis.etadetroit.datalayer.repository.Repository;
+import riis.etadetroit.presentationlayer.view.CompanyDetailsActivity;
+import riis.etadetroit.presentationlayer.view.RouteDetailsActivity;
+
+public class CallNextViewInteractor {
+
+    private Context context;
+
+    public CallNextViewInteractor(Context context) {
+        this.context = context;
+    }
+
+    public void callNextView(View v, int position, Context mainActivityContext) {
+        Repository repository = new Repository(context);
+        if (repository.getCompanyName(position).equals(context.getString(R.string.people_mover))) {
+            Intent intent = new Intent(context, RouteDetailsActivity.class);
+            intent.putExtra(RouteDetailsActivity.EXTRA_ROUTE, context.getString(R.string.people_mover));
+            context.startActivity(intent);
+        } else {
+            Intent intent = new Intent(context, CompanyDetailsActivity.class);
+            intent.putExtra(CompanyDetailsActivity.EXTRA_PARAM_ID, position);
+            ImageView busImage = (ImageView) v.findViewById(R.id.busImage);
+            LinearLayout busNameHolder = (LinearLayout) v.findViewById(R.id.busNameHolder);
+
+
+            Pair<View, String> imagePair = Pair.create((View) busImage, "tImage");
+            Pair<View, String> holderPair = Pair.create((View) busNameHolder, "tNameHolder");
+
+            ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation((Activity) mainActivityContext, imagePair, holderPair);
+            ActivityCompat.startActivity((Activity) mainActivityContext, intent, options.toBundle());
+        }
+    }
+}
--- app/src/main/java/riis/etadetroit/presentationlayer/presenter/MainActivityPresenter.java
+++ app/src/main/java/riis/etadetroit/presentationlayer/presenter/MainActivityPresenter.java
@@ -1,21 +1,12 @@
 package riis.etadetroit.presentationlayer.presenter;
 
-import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.ActivityOptionsCompat;
-import android.support.v4.util.Pair;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 
+import riis.etadetroit.domainlayer.interactor.CallNextViewInteractor;
 import riis.etadetroit.domainlayer.interactor.CompanyImageIDInteractor;
 import riis.etadetroit.domainlayer.interactor.CompanyListSizeInteractor;
 import riis.etadetroit.domainlayer.interactor.CompanyNameInteractor;
-import riis.etadetroit.presentationlayer.view.CompanyDetailsActivity;
-import riis.etadetroit.presentationlayer.view.RouteDetailsActivity;
-import riis.etadetroit.R;
 
 
 public class MainActivityPresenter {
@@ -42,22 +33,7 @@ public class MainActivityPresenter {
     }
 
     public void callNextView(View v, int position, Context mainActivityContext) {
-        if (getCompanyName(position).equals(context.getString(R.string.people_mover))) {
-            Intent intent = new Intent(context, RouteDetailsActivity.class);
-            intent.putExtra(RouteDetailsActivity.EXTRA_ROUTE, context.getString(R.string.people_mover));
-            context.startActivity(intent);
-        } else {
-            Intent intent = new Intent(context, CompanyDetailsActivity.class);
-            intent.putExtra(CompanyDetailsActivity.EXTRA_PARAM_ID, position);
-            ImageView busImage = (ImageView) v.findViewById(R.id.busImage);
-            LinearLayout busNameHolder = (LinearLayout) v.findViewById(R.id.busNameHolder);
-
-
-            Pair<View, String> imagePair = Pair.create((View) busImage, "tImage");
-            Pair<View, String> holderPair = Pair.create((View) busNameHolder, "tNameHolder");
-
-            ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation((Activity) mainActivityContext, imagePair, holderPair);
-            ActivityCompat.startActivity((Activity) mainActivityContext, intent, options.toBundle());
-        }
+        CallNextViewInteractor callNextViewInteractor = new CallNextViewInteractor(context);
+        callNextViewInteractor.callNextView(v, position, mainActivityContext);
     }
 }
