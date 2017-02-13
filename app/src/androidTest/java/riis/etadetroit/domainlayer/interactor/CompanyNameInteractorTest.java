3a4d4d39d6fd88f090157a358b7bae56dd7bbd49
Brian Marshall <bmarshall@RIIS.com>
2017-02-13 13:48:00 -0500

    created test dependencies

---
+++ app/src/androidTest/java/riis/etadetroit/domainlayer/interactor/CompanyNameInteractorTest.java
@@ -0,0 +1,16 @@
+package riis.etadetroit.domainlayer.interactor;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by bmarshall on 2/13/17.
+ */
+public class CompanyNameInteractorTest {
+    @Test
+    public void getCompanyName() throws Exception {
+        assertEquals(true, true);
+    }
+
+}
